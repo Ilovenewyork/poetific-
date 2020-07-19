2cbddad4cbb08bd5fb920acce5c91f4e48662160
Ilovenewyork <31485037+Ilovenewyork@users.noreply.github.com>
2020-07-19 07:26:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+var modal = document.getElementsByClassName("modal");
+var btn = document.getElementsByClassName("container");
+var cncl = document.getElementById("cancel")
+
+btn[0].onclick = function() {
+  modal[0].style.display = "block";
+  cancel.style.display = "block";
+}
+
+btn[1].onclick = function() {
+  modal[1].style.display = "block";
+  cancel.style.display = "block";
+}
+
+btn[2].onclick = function() {
+  modal[2].style.display = "block";
+  cancel.style.display = "block";
+}
+
+btn[3].onclick = function() {
+  modal[3].style.display = "block";
+cancel.style.display = "block";
+}
+
+btn[4].onclick = function() {
+  modal[4].style.display = "block";
+cancel.style.display = "block";
+}
+
+btn[5].onclick = function() {
+  modal[5].style.display = "block";
+cancel.style.display = "block";
+}
+
+btn[6].onclick = function() {
+  modal[6].style.display = "block";
+cancel.style.display = "block";
+}
+
+btn[7].onclick = function() {
+  modal[7].style.display = "block";
+cancel.style.display = "block";
+cancel.style.display = "block";
+}
+
+btn[8].onclick = function() {
+  modal[8].style.display = "block";
+cancel.style.display = "block";
+}
+
+
+btn[9].onclick = function() {
+  modal[9].style.display = "block";
+cancel.style.display = "block";
+}
+
+btn[10].onclick = function() {
+  modal[10].style.display = "block";
+cancel.style.display = "block";
+}
+
+btn[11].onclick = function() {
+  modal[11].style.display = "block";
+cancel.style.display = "block";
+}
+window.onkeydown||cancel.onclick =function(){
+  modal[1].style.display="none";
+  modal[0].style.display="none";
+  modal[2].style.display="none";
+  modal[3].style.display="none";
+  modal[4].style.display="none";
+  modal[5].style.display="none";
+    modal[6].style.display="none";
+    modal[7].style.display="none";
+    modal[8].style.display="none";
+    modal[9].style.display="none";
+    modal[10].style.display="none";
+    modal[11].style.display="none";
+    cancel.style.display="none";
+}
