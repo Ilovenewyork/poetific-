eb83b3b1307f11ea729caf9ba631a04fd6fd6754
Ilovenewyork <31485037+Ilovenewyork@users.noreply.github.com>
2020-07-21 13:33:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
 var modal = document.getElementsByClassName("modal");
 var btn = document.getElementsByClassName("container");
 
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 
 btn[0].onclick = function() {
   modal[0].style.display = "block";
