1bd7ffa4bcd7c32ab780e6a3f24bf50ed9aa36e2
Ilovenewyork <31485037+Ilovenewyork@users.noreply.github.com>
2020-08-06 13:12:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,48 +17,107 @@ btn[1].onclick = function() {
   modal[1].style.display = "block";
 }
 
+cancel[1].onclick = function() {
+  modal[1].style.display = "none";
+  
+}
+
 btn[2].onclick = function() {
   modal[2].style.display = "block";
+}
+
+cancel[2].onclick = function() {
+  modal[2].style.display = "none";
+  
 }
 
 btn[3].onclick = function() {
   modal[3].style.display = "block";
 }
 
+cancel[3].onclick = function() {
+  modal[3].style.display= "none";
+  
+}
+
 btn[4].onclick = function() {
   modal[4].style.display = "block";
+}
+
+cancel[4].onclick = function() {
+  modal[4].style.display = "none";
+  
 }
 
 btn[5].onclick = function() {
   modal[5].style.display = "block";
 }
 
+cancel[5].onclick = function() {
+  modal[5].style.display = "none";
+  
+}
+
 btn[6].onclick = function() {
   modal[6].style.display = "block";
+}
+
+cancel[6].onclick = function() {
+  modal[6].style.display = "none";
+  
 }
 
 btn[7].onclick = function() {
   modal[7].style.display = "block";
 }
 
+cancel[7].onclick = function() {
+  modal[7].style.display = "none";
+  
+}
+
 btn[8].onclick = function() {
   modal[8].style.display = "block";
 }
 
+cancel[8].onclick = function() {
+  modal[8].style.display = "none";
+  
+}
 
 btn[9].onclick = function() {
   modal[9].style.display = "block";
 }
 
+cancel[9].onclick = function() {
+  modal[9].style.display = "none";
+  
+}
 btn[10].onclick = function() {
   modal[10].style.display = "block";
+}
+
+cancel[10].onclick= function() {
+  modal[10].style.display= "none";
+  
 }
 
 btn[11].onclick = function() {
   modal[11].style.display = "block";
 }
+
+cancel[11].onclick= function() {
+  modal[11].style.display= "none";
+  
+}
+
 btn[12].onclick = function() {
   modal[12].style.display = "block";
+}
+
+cancel[12].onclick= function() {
+  modal[12].style.display= "none";
+  
 }
 window.onkeydown=function(){
   modal[1].style.display="none";
